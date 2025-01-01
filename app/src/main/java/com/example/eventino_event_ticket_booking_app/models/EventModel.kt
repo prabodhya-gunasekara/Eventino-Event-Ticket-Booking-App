a2b87e97b9838c3ed981e149b40190e1fffbe9ea
Chithmi Dilaksha <148516815+ChithmiDilaksha@users.noreply.github.com>
2025-01-01 13:09:06 +0530

    Create event model data class

---
+++ app/src/main/java/com/example/eventino_event_ticket_booking_app/models/EventModel.kt
@@ -0,0 +1,6 @@
+package com.example.eventino_event_ticket_booking_app.models
+
+data class EventModel(
+    val eventName: String = "", val eventID: String = "", val date: String = "", val time: String = "",
+    val location: String = "", val imgURL: String = ""
+)
