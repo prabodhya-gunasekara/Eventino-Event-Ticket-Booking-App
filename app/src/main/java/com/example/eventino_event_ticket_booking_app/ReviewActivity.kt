0082a2b7d0de337617c04f024bb52811cec61a8c
kalanisithu <sithumininanayakkara4@gmail.com>
2025-01-04 15:29:05 +0530

    Review Activity

--- app/src/main/java/com/example/eventino_event_ticket_booking_app/ReviewActivity.kt
+++ app/src/main/java/com/example/eventino_event_ticket_booking_app/ReviewActivity.kt
@@ -1,20 +1,19 @@
 package com.example.eventino_event_ticket_booking_app
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class ReviewActivity : AppCompatActivity() {
+class AddReviewActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_review)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+        findViewById<Button>(R.id.submitButton).setOnClickListener {
+            Toast.makeText(this, "Review submitted successfully!", Toast.LENGTH_SHORT).show()
         }
     }
 }
